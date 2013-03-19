fc4695a2bac51a4e2ba5b176cc2bee927bbdb496
picasso250 <cumt.xiaochi@gmail.com>
2013-03-19 17:26:46 +0800

    add _isset() __set() 方法 将 Searcher 类也集中到这里来。

--- core.model.php
+++ core.model.php
@@ -1,10 +1,14 @@
 <?php
 
 /**
+ * 
+ * ORM for LazyPHP
+ * 
+ * https://github.com/picasso250/ORM4LazyPHP
+ * 
+ * 这是一个为 LazyPHP 打造的
  * @author picasso250
  */
-
-require_once CROOT.'model'.DS.'core.searcher.php';
 
 class CoreModel
 {
@@ -121,6 +125,22 @@ class CoreModel
         return $this->info[$name];
     }
 
+    public function __set($prop, $value)
+    {
+        $this->update($prop, $value);
+    }
+
+    public function __isset($prop)
+    {
+        $this->info();
+        return isset($this->info[$prop]);
+    }
+
+    public function get($prop)
+    {
+        return $this->__get($prop);
+    }
+
     public function __call($name, $args)
     {
         if (empty($this->info)) {
@@ -153,5 +173,138 @@ class CoreModel
     {
         $self = get_called_class();
         return isset($self::$relationMap) ? $self::$relationMap : array();
+    }
+}
+
+class Searcher
+{
+    private $table = null;
+    private $class = null;
+    private $tables = array();
+    private $conds = array();
+    private $orders = array();
+    private $limit = 1000;
+    private $offset = 0;
+    private $distinct = false;
+    
+    public function __construct($class)
+    {
+        $this->class = $class;
+        $this->table = $class::table();
+        $this->tables[] = $this->table;
+    }
+
+    public function table()
+    {
+        return $this->table;
+    }
+
+    public function filterBy($exp, $value, $op = '=')
+    {
+        // 使得用户可以传一个object进来
+        // is_object() 判断不可少，不然SAE上会把String也认为Ojbect
+        if (is_object($value) && is_a($value, 'CoreModel'))
+            $value = $value->id;
+
+        $relationMap = $this->relationMap();
+        $tableDotKey = preg_match('/\b(\w+)\.(\w+)\b/', $exp, $matches); // table.key = ?
+        $tableDotId = isset($relationMap[$exp]);
+
+        if ($tableDotKey) {
+            $ref = $matches[1];
+            $refKey = $matches[2];
+            $refTable = $relationMap[$ref];
+            $this->conds["$refTable.$refKey=?"] = $value;
+            $this->conds["$this->table.$ref=$refTable.id"] = null;
+        } else {
+            if (strpos($exp, '?') === false && $value !== null) {
+                $exp = "$this->table.$exp=?";
+            }
+            $this->conds[$exp] = $value;
+        }
+            
+        return $this;
+    }
+
+    public function orderBy($exp)
+    {
+        $this->orders[] = "$this->table.$exp";
+        return $this;
+    }
+
+    public function limit()
+    {
+        if (!func_num_args())
+            return $this->limit;
+        $this->limit = func_get_arg(0);
+        return $this;
+    }
+
+    public function offset()
+    {
+        if (!func_num_args())
+            return $this->offset;
+        $this->offset = func_get_arg(0);
+        return $this;
+    }
+
+    public function join(Searcher $s)
+    {
+        $st = $s->table();
+        $rMap = array_flip($s->relationMap());
+        $refKey = $rMap[$this->table];
+        $this->conds[$st . ".$refKey=$this->table.id"] = null;
+        $this->conds += $s->conds;
+
+        if (!in_array($st, $this->tables))
+            $this->tables[] = $st;
+        return $this;
+    }
+
+    public function distinct()
+    {
+        $this->distinct = true;
+        return $this;
+    }
+
+    public function find()
+    {
+        $field = "$this->table.id";
+        if ($this->distinct)
+            $field = "DISTINCT($field)";
+        $limitStr = $this->limit ? "LIMIT $this->limit" : '';
+        $tail = "$limitStr OFFSET $this->offset";
+        if ($this->conds) {
+            $condStr = implode(' AND ', array_keys($this->conds));
+            $a = array_filter(array_values($this->conds));
+            $values = array();
+            foreach ($a as $v) {
+                if (is_array($v)) {
+                    $values += $v;
+                } else {
+                    $values[] = $v;
+                }
+            }
+            $conds = array($condStr => $values);
+        } else {
+            $conds = '';
+        }
+        $ids = Sdb::fetch($field, $this->tables, $conds, $this->orders, $tail);
+
+        $class = $this->class;
+        $ret = array_map(function ($id) use($class) {
+            return new $class($id);
+        }, $ids);
+        return $ret;
+    }
+
+    // ------------ private section -----------------
+
+    private function relationMap()
+    {
+        if (isset($this->relationMap))
+            return $this->relationMap;
+        $class = $this->class;
+        return $this->relationMap = $class::relationMap();
     }
 }
